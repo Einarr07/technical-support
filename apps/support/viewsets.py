231b77575734b551d4ad1c43757d748e16900413
Einarr07 <aidenperse@gmail.com>
2026-03-15 19:33:59 -0500

    Authentication and permissions

--- apps/support/viewsets.py
+++ apps/support/viewsets.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from apps.support.models import Ticket
+from apps.support.permissions import IsTechnical
 from apps.support.serializers import TicketSerializer
 
 
@@ -10,9 +12,11 @@ class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
 
+    permission_classes = [IsAuthenticated]
+
     # detail=True meaning than the URL need ID (ej: /tickets/5/resolver/)
     # If was detail=False, be as (ej: /tickets/resueltos/)
-    @action(detail=True, methods=['post'], url_path='ticket-solved')
+    @action(detail=True, methods=['post'], url_path='ticket-solved', permission_classes=[IsAdminUser | IsTechnical])
     def ticket_solved(self, request, pk=None):
         # 1. Get specific ticket
         ticket = self.get_object()
@@ -27,7 +31,7 @@ class TicketViewSet(viewsets.ModelViewSet):
             'ticket_code': ticket.code,
         }, status=status.HTTP_200_OK)
 
-    @action(detail=True, methods=['post'], url_path='ticket-unsolved')
+    @action(detail=True, methods=['post'], url_path='ticket-unsolved', permission_classes=[IsAdminUser | IsTechnical])
     def ticket_unsolved(self, request, pk=None):
         # 1. Get specific ticket
         ticket = self.get_object()
--- technical_support/urls.py
+++ technical_support/urls.py
@@ -20,6 +20,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/', include('apps.core.urls')),
     path('api/', include('apps.support.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
